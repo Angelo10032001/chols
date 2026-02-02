acb927b43214404db9963f839778ffdd306d3077
Angelo10032001 <68455109+Angelo10032001@users.noreply.github.com>
2026-02-03 02:30:59 +0800

    Delete models directory

--- models/Promo.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const PromoSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  days: { type: Number, required: true }
-});
-
-export default mongoose.model("Promo", PromoSchema);
--- models/Transaction.js
+++
@@ -1,8 +0,0 @@
-const TransactionSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  rfid: String,
-  amount: Number,
-  days: Number,
-  method: String,
-  createdAt: { type: Date, default: Date.now }
-});
